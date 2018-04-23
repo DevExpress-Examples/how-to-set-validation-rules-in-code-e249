b8c3fc2bc32a8f796f8a73d039abd90fc43843b2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToSetValidationRulesInCode.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace HowToSetValidationRulesInCode.Module {
+	public sealed partial class HowToSetValidationRulesInCodeModule : ModuleBase {
+		public HowToSetValidationRulesInCodeModule() {
+			InitializeComponent();
+		}
+	}
+}
