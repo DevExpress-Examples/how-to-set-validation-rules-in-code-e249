45887f7782ad2cd62eb66815e4ad7edce0bd89c4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToSetValidationRulesInCode.Module.Web/WebModule.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace HowToSetValidationRulesInCode.Module.Web {
+	[ToolboxItemFilter("Xaf.Platform.Web")]
+	public sealed partial class HowToSetValidationRulesInCodeAspNetModule : ModuleBase {
+		public HowToSetValidationRulesInCodeAspNetModule() {
+			InitializeComponent();
+		}
+	}
+}
